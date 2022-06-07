14b050261821cc0dc7fda0e01e201164fe3e0a13
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2022-06-07 12:01:12 -0700

    foreground service, use gson properly

--- app/src/main/java/tk/donkeyblaster/ftxpricenotification/NotificationService.java
+++ app/src/main/java/tk/donkeyblaster/ftxpricenotification/NotificationService.java
@@ -3,6 +3,7 @@ package tk.donkeyblaster.ftxpricenotification;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -10,7 +11,10 @@ import android.os.IBinder;
 import android.util.Log;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.internal.LinkedTreeMap;
 import com.google.gson.reflect.TypeToken;
 import com.neovisionaries.ws.client.WebSocket;
@@ -23,7 +27,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 public class NotificationService extends Service {
@@ -33,40 +36,35 @@ public class NotificationService extends Service {
     String FTX_API_URI = "wss://ftx.com/ws/";
 
     NotificationManager notificationManager;
-    Notification.Builder notificationBuilder;
+    Notification notification;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("start", "start");
         notificationManager = getNotificationManager();
         createNotificationChannel(notificationManager);
-        notificationBuilder = getNotificationBuilder();
+        notification = getNewNotification("Starting...");
 
         // No networking on main thread over here
         Thread wsThread = new Thread() {
             @Override
             public void run() {
                 super.run();
-                runWebsocketLoop(notificationManager, notificationBuilder);
+                runWebsocketLoop(notificationManager, notification);
             }
         };
         wsThread.start();
 
+        startForeground(NOTIFICATION_ID, notification);
+
         return START_NOT_STICKY;
     }
 
-    private void runWebsocketLoop(NotificationManager nm, Notification.Builder nb) {
+    private void runWebsocketLoop(NotificationManager nm, Notification n) {
         WebSocketFactory factory = new WebSocketFactory();
         factory.setConnectionTimeout(5000); // 5s
         //factory.setVerifyHostname(false);
         WebSocket ws = null;
-
-        //ArrayList<Ticker> tickers = Ticker.tickers; // copy to local since the upstream copy can change
-        // LinkedHashMap<Ticker, NotionalValue>
-        LinkedHashMap<String, Ticker> tickers = new LinkedHashMap<>();
-        for (Ticker t : Ticker.tickers) {
-            tickers.put(t.getTicker(), t);
-        }
 
         try {
             ws = factory.createSocket(FTX_API_URI);
@@ -83,17 +81,21 @@ public class NotificationService extends Service {
 
         if (ws == null) return;
 
-        ws.addListener(new WebSocketAdapter() {
-            @Override
-            public void onConnected(WebSocket websocket, Map<String, List<String>> headers) throws Exception {
-                super.onConnected(websocket, headers);
-                changeSubscription(websocket, tickers, false);
-            }
+        //ArrayList<Ticker> tickers = Ticker.tickers; // copy to local since the upstream copy can change
+        // LinkedHashMap<Ticker, NotionalValue>
+        LinkedHashMap<String, Ticker> tickers = new LinkedHashMap<>();
+        for (Ticker t : Ticker.tickers) {
+            tickers.put(t.getTicker(), t);
+        }
 
+        ws.addListener(new WebSocketAdapter() {
             @Override
             public void onTextMessage(WebSocket websocket, String text) throws Exception {
                 super.onTextMessage(websocket, text);
-                String displayedContent = getTicker(text) + ": " + getPrice(text);
+                Log.d("WS message recvd", text);
+                Double price = getPrice(text);
+                String displayedContent = getTicker(text) + ": " + price;
+                Log.d("displayedContent", displayedContent);
                 float positionSize = tickers.get(getTicker(text)).getPositionSize();
                 if (positionSize > 0) { // Long
                     double priceDiff = getPrice(text) - tickers.get(getTicker(text)).getEntryPrice();
@@ -102,71 +104,41 @@ public class NotificationService extends Service {
                     double priceDiff = tickers.get(getTicker(text)).getEntryPrice() - getPrice(text);
                     displayedContent += " // PnL: " + priceDiff * positionSize;
                 }
-
-                editNotificationText(nb, nm, displayedContent);
-
-            }
-
-            @Override
-            public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) throws Exception {
-                super.onDisconnected(websocket, serverCloseFrame, clientCloseFrame, closedByServer);
-                changeSubscription(websocket, tickers, true);
+                Log.d("attempt", "displaying notification");
+                displayNotification(notificationManager, getNewNotification(displayedContent));
             }
 
             @Override
             public void onSendError(WebSocket websocket, WebSocketException cause, WebSocketFrame frame) throws Exception {
                 super.onSendError(websocket, cause, frame);
                 Log.e("NotificationService#runWebsocketLoop.onSendError", cause.toString());
-                editNotificationText(nb, nm, "Error:" + cause);
+                displayNotification(notificationManager, getNewNotification(cause.toString()));
             }
 
             @Override
             public void onError(WebSocket websocket, WebSocketException cause) throws Exception {
                 super.onError(websocket, cause);
                 Log.e("NotificationService#runWebsocketLoop.onError", cause.toString());
-                editNotificationText(nb, nm, "Error:" + cause);
+                displayNotification(notificationManager, getNewNotification(cause.toString()));
             }
         });
+        changeSubscription(ws, tickers, false);
     }
 
     private String getTicker(String rawData) {
         if (rawData == null) return null;
 
-        Map<String, Object> data = new Gson().fromJson(rawData, new TypeToken<HashMap<String, Object>>() {}.getType());
-
-        try {
-            return String.valueOf(data.get("market"));
-        } catch (Exception e) {
-            Log.w("NotificationService#getPrice", e.toString());
-            e.printStackTrace();
-            return null;
-        }
+        JsonObject jsonObject = JsonParser.parseString(rawData).getAsJsonObject();
+        return jsonObject.get("market").getAsString();
     }
 
-    @SuppressWarnings("unchecked")
     private Double getPrice(String rawData) {
         if (rawData == null) return null;
 
-        Map<String, Object> data = new Gson().fromJson(rawData, new TypeToken<HashMap<String, Object>>() {}.getType());
-        LinkedTreeMap<String, String> result;
-
-        try {
-            ArrayList<LinkedTreeMap<String, String>> temp = (ArrayList<LinkedTreeMap<String, String>>) data.get("data");
-            result = temp.get(0);
-            Log.d("asdf", String.valueOf(result.get("price")));
-        } catch (Exception e) {
-            Log.w("NotificationService#getPrice", e.toString());
-            e.printStackTrace();
-            return null;
-        }
-
-        try {
-            return Double.parseDouble(result.get("price"));
-        } catch (NullPointerException e) {
-            Log.w("NotificationService#getPrice", e.toString());
-            e.printStackTrace();
-            return null;
-        }
+        JsonObject jsonObject = JsonParser.parseString(rawData).getAsJsonObject();
+        JsonArray jsonArray = jsonObject.getAsJsonArray("data");
+        JsonObject finalObject = jsonArray.get(0).getAsJsonObject();
+        return finalObject.get("price").getAsDouble();
     }
 
     private void changeSubscription(WebSocket ws, LinkedHashMap<String, Ticker> tickers, boolean unsubscribe) {
@@ -180,18 +152,24 @@ public class NotificationService extends Service {
         }
     }
 
-    private void editNotificationText(Notification.Builder nb, NotificationManager nm, String message) {
-        nb.setContentText(message);
-        nm.notify(NOTIFICATION_ID, nb.build());
+    private void displayNotification(NotificationManager nm, Notification n) {
+        nm.notify(NOTIFICATION_ID, n);
     }
 
-    private Notification.Builder getNotificationBuilder() {
+    private Notification getNewNotification(String content) {
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        // PendingIntent launches activity when user taps the notification
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+
         return new Notification.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_baseline_show_chart_48)
                 .setContentTitle("FTX Perpetual Contract Prices")
                 .setOnlyAlertOnce(true)
                 .setOngoing(true)
-                .setUsesChronometer(true);
+                .setUsesChronometer(true)
+                .setContentIntent(pendingIntent)
+                .setContentText(content)
+                .build();
     }
 
     private NotificationManager getNotificationManager() {
